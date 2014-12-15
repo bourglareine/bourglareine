1e8a57053095f01b104c9be9a7f374b3d326d782
bourglareine <projectbourglareine@gmail.com>
2014-12-15 23:59:06 +0100

    Ajout animation courir + Saut+ Double Saut
    Remarque: Karim, j'ai changé les valeurs de jumpForce et j'ai ajouté des
    limitations sur le double saut. T'es libre de tweaker comme il te plait
    ;).

--- Cubic/Assets/2DTile/Demo/Scripts/Player.cs
+++ Cubic/Assets/2DTile/Demo/Scripts/Player.cs
@@ -1,49 +1,67 @@
-		using UnityEngine;
+using UnityEngine;      
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float maxSpeed=20f;
+	bool facingRight = true;
+	bool grounded=false;
+	public Transform groundCheck;
+	float groundRadius =0.2f;
+	public LayerMask whatIsGround;
+	public float jumpForce =500;
+	bool doubleJump = false;
+
+	Animator anim;
 
 
-		/// <summary>
-		/// Player controller and behavior
-		/// </summary>
-		public class Player : MonoBehaviour
+	void Start()
+	{
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update()
+	{
+		if ((grounded || !doubleJump) && Input.GetKeyDown (KeyCode.Space)) 
 		{
-		    /// <summary>
-		    /// 1 - The speed of the ship
-		    /// </summary>
-		    public Vector2 speed = new Vector2(50, 5);
-		    public float jumpSpeed = 500.0f;
-		    public bool IsGrounded = false;
-		    public bool Jumping = false;
-		    public bool inAir = false;
-		    public float airControl = 0.5f;
-			    bool facingRight=true;
+			anim.SetBool ("ground",false);
+			rigidbody2D.AddForce (new Vector2(0,doubleJump?jumpForce/2:jumpForce),ForceMode2D.Impulse);
 
+			if (!doubleJump && !grounded)
+			{doubleJump=true;}
+		}
+	}
 
-		    // 2 - Store the movement
-		    private Vector2 movement;
-		    private Vector2 force;
-		    private Vector2 actualForce;
-		    private Vector2 upTranform2d = new Vector2(0, 1.0f);
-		    private Vector2 jumpVector;
-			private RaycastHit2D hit;
+	void FixedUpdate()
+	{
 
-				    void FixedUpdate()
-					    {
-					        Movement();
-					    }
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		anim.SetBool("ground", grounded);
 
-		    void Movement()
-		    {
+		anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
+
+		if (grounded) {
+			doubleJump=false;}
+
+		// if (!grounded) return; // non maniabilité du joueur en air  
+
 		float move = Input.GetAxis ("Horizontal");
-				rigidbody2D.velocity= new Vector2(move*speed.x,rigidbody2D.velocity.y);
-		if (move > 0 && !facingRight) {
-						Flip ();
-				} else if (move < 0 && facingRight) {
-				Flip ();
-				}
-		    }
 
-	void Flip(){
+		anim.SetFloat("speed", Mathf.Abs(move));
+		// "Opérateur ternaire"(cf.google pour le terme):a partie du code : doubleJumpe?0.4f:1) est équivalente à : if(doubleJump){valeur=0.4}else{valeur=1} et valeur est injectée directement dans le code 
+			rigidbody2D.velocity = new Vector2 (move * maxSpeed*(doubleJump?0.4f:1), rigidbody2D.velocity.y);
+
+		if (move > 0 && !facingRight) 
+						Flip ();
+				else if (move < 0 && facingRight)
+						Flip ();
+	}
+
+	void Flip()
+	{
 		facingRight = !facingRight;
-		transform.localScale = new Vector3 (transform.localScale.x*-1, transform.localScale.y, transform.localScale.z);
-		}
-		}
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
